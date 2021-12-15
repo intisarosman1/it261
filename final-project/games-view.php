ecbf7f3bf9ac3874ddd02e4f925342c31e44401e
intisarosman1 <77607212+intisarosman1@users.noreply.github.com>
2021-12-14 17:08:37 -0800

    Update games-view.php

--- final-project/games-view.php
+++ final-project/games-view.php
@@ -1,7 +1,47 @@
-<?php // people-view.php
+<?php // games-view.php
+
+session_start();
+
 include('config.php');
 
 
+if(!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = 'You must login first!';
+    header('Location:login.php');
+}
+
+if(isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location:login.php');
+}
+
+include('includes/header.php');
+
+if(isset($_SESSION['success'])) :?>
+
+<div class="success">
+<h3>
+<?php echo $_SESSION['success'];
+    unset($_SESSION['success']);
+?>
+</h3>
+</div>
+
+<?php endif;
+
+if(isset($_SESSION['username'])) :?>
+<div class="welcome-logout">
+<h3>Hello
+<?php echo $_SESSION['username']; ?>!
+</h3>
+<p><a href="index.php?logout='1' ">Log out</a></p>
+</div>
+<?php endif; ?>
+
+</header>
+
+<?php
 if(isset($_GET['id'])) {
 $id = (int)$_GET['id'];
 
@@ -32,7 +72,7 @@ $feedback = 'Something is not working!';
 }
 
 // for the big assignment you will now call out the header include
-include('includes/header.php');
+//include('includes/header.php');
 
 ?>
 
